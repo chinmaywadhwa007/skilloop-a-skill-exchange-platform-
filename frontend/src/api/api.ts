03d6fbd7e4c0d912df0e9cc197db71a7c9744d5c
Chinmay Wadhwa <201734343+chinmaywadhwa007@users.noreply.github.com>
2026-08-01 16:57:44 +0530

    update api updated

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5001/api/v1",
+  baseURL: "https://skilloop-a-skill-exchange-platform-5.onrender.com/api/v1",
   withCredentials: true,
   timeout: 10000,
   headers: {
@@ -19,7 +19,7 @@ api.interceptors.request.use(
 
     return config;
   },
-  (error) => Promise.reject(error)
+  (error) => Promise.reject(error),
 );
 
 export default api;
